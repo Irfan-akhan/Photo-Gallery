0718fff016b22d55759584c291cca06a051ad525
Irfan Khan <officialirfanafzal@gmail.com>
2020-08-29 16:46:08 +0530

    added feed

--- javascript/app.js
+++ javascript/app.js
@@ -1,32 +1,41 @@
 const ACCESS_KEY = 'y3c0XtvLz3mNxzIRLyqws0Lwo6dywBT8Toa4A2eODFE';
-const BASE_URL = `https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`;
+const BASE_URL = `https://api.unsplash.com/search/photos/?per_page=30&query=wild&client_id=${ACCESS_KEY}`;
+let counter = 0;
 
-const newSectionImgs = document
-    .getElementById('new')
-    .lastElementChild.querySelectorAll('img');
+const newSectionImgs = document.querySelector('#new div');
 console.log(newSectionImgs);
 
-let counter = 0;
+const mainElement = document.querySelector('main');
+console.log(mainElement);
+
 console.log(BASE_URL);
+
 const createImgs = imgUrl => {
-    newSectionImgs[counter].src = imgUrl;
-    counter++;
+    // newSectionImgs[counter].src = imgUrl;
+    // counter++;
+    let imgEl = document.createElement('img');
+    imgEl.src = imgUrl;
+    newSectionImgs.appendChild(imgEl);
+    mainElement.appendChild(imgEl);
 };
-const printImgs = images => {
-    images.forEach(imgUrl => {
+const printImgs = imagesUrls => {
+    imagesUrls.forEach(imgUrl => {
         createImgs(imgUrl);
     });
 };
 const extractImgsData = data => {
     console.log(data);
-    images = data
+    imageUrls = data
         .map(obj => {
             return obj.urls;
         })
-        .map(url => url.regular);
-    console.log(images);
-    printImgs(images);
+        .map(url => url.small);
+    console.log(imageUrls);
+    printImgs(imageUrls);
 };
 fetch(BASE_URL)
     .then(res => res.json())
-    .then(data => extractImgsData(data));
+    .then(data => {
+        console.log('my data', data);
+        extractImgsData(data.results);
+    });
